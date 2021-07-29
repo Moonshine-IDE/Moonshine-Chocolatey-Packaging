2ed8106bc3364fd13ed97cb6eea9576f9d2ed6af
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2021-07-29 20:09:23 +0200

    Create moonshine-ide packaging scripts

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocaction = Join-Path $toolsDir 'Moonshine-3.1.1.exe'
+$packageName = 'Moonshine-IDE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  file         	= $fileLocaction
+  softwareName  = 'Moonshine'
+
+  # MSI
+  silentArgs    = "/S" #NSIS
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
